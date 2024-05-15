586e1c9143619d954736d008c89f6cee772fbb3f
rana5679 <124688927+rana5679@users.noreply.github.com>
2024-05-15 21:51:23 +0300

    Update Time.v

--- Source Files/Time.v
+++ Source Files/Time.v
@@ -6,22 +6,22 @@ module Time (input clk, rst, adjust, ENTH, ENTM, updown, output [1:0] H1, [3:0] 
 // updown set before depending on mode
 
 wire enm, enh;
-wire  [5:0] secs;
+wire  [5:0] sec;
 wire  [5:0] mins;
 wire  [4:0] hours;
 
-Up_Down_Mod_Counter #(6,60) s (.clk(clk), .rst(rst), .en(~adjust),.upDown(0), .count(secs));
+Up_Down_Mod_Counter #(6,60) s (.clk(clk), .rst(rst), .en(~adjust),.upDown(0), .count(sec));
 Up_Down_Mod_Counter #(6,60) m (.clk(clk), .rst(rst), .en(enm),.upDown(updown), .count(mins));
 Up_Down_Mod_Counter #(5,24) h (.clk(clk), .rst(rst), .en(enh), .upDown(updown),.count(hours));
 
-assign enm = adjust ?  ENTM : secs == 59;
-assign enh = adjust ?  ENTH : mins == 59 & secs == 59;
+assign enm = adjust ?  ENTM : sec == 59;
+assign enh = adjust ?  ENTH : mins == 59 & sec == 59;
 
 assign H1 = hours / 10;
 assign H2 = hours % 10;
 assign M1 = mins / 10;
 assign M2 = mins % 10;
 
-if (adjust == 1) assign secs = 0;
+assign sec = adjust ? 0 : sec==59;
 
 endmodule
